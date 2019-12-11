2f2111a379c42790c8c3b3c43ca1296bac3cec30
Cabaret Pierre-Antoine <pierreant.cab@gmail.com>
2019-12-11 10:18:07 +0100

    src

---
+++ include/squares.hpp
@@ -0,0 +1,26 @@
+//
+// Created by picabare on 04/12/19.
+//
+
+#ifndef PROJET_OPENCV_CMAKE_SQUARES_HPP
+#define PROJET_OPENCV_CMAKE_SQUARES_HPP
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+
+using namespace cv;
+using namespace std;
+
+double angle( Point pt1, Point pt2, Point pt0 );
+void findSquares( const Mat& image, vector<vector<Point> >& squares,  int thresh = 50, int N = 5 );
+void drawSquares( Mat& image, const vector<Rect>& rectangles);
+void pruneSquares(vector<vector<Point> >& squares, vector<Rect>& rectangles, int dist = 2);
+
+
+#endif //PROJET_OPENCV_CMAKE_SQUARES_HPP
---
+++ src/main_test_opencv.cpp
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////////////
+// Option Images
+// Projet, séance 1
+// thème : premier pas en OpenCV
+// contenu : charge, affiche, réduction, calcul et affichage d'histogramme
+// version : 17.1128
+//////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include "squares.hpp"
+
+using namespace std;
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+using namespace cv;
+
+#include "histogram.hpp"
+
+
+int main (void) {
+
+    vector<string> names;
+
+    names.push_back("../00000.png");
+
+//    namedWindow( "oui", 1 );
+    vector<vector<Point>> squares;
+    vector<Rect> rectangles;
+
+    for( int i = 0; i < names.size(); i++ )
+    {
+        Mat image = imread(names[i], 1);
+        if( image.empty() )
+        {
+            cout << "Couldn't load " << names[i] << endl;
+            continue;
+        }
+
+        findSquares(image, squares);
+        cout << squares.size() << endl;
+        pruneSquares(squares, rectangles);
+        drawSquares(image, rectangles);
+        imwrite( "out.png", image );
+        int c = waitKey();
+        if( (char)c == 27 )
+            break;
+    }
+	
+	return EXIT_SUCCESS;
+}
